8e68dd1cdf37b6760052412a25a0017413a0a1ef
Jonathan Rigaux <jonathan.rigaux@rheinschafe.de>
2026-02-06 15:22:41 +0100

    Clean up provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,28 +4,7 @@ namespace Narsil\Cms\Form;
 
 #region USE
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
-use Narsil\Cms\Form\Providers\CommandServiceProvider;
-use Narsil\Cms\Form\Providers\ConfigurationServiceProvider;
-use Narsil\Cms\Form\Providers\FieldServiceProvider;
-use Narsil\Cms\Form\Providers\FormRequestServiceProvider;
-use Narsil\Cms\Form\Providers\FormServiceProvider;
-use Narsil\Cms\Form\Providers\FortifyServiceProvider;
-use Narsil\Cms\Form\Providers\HorizonServiceProvider;
-use Narsil\Cms\Form\Providers\LocalizationServiceProvider;
-use Narsil\Cms\Form\Providers\MenuServiceProvider;
-use Narsil\Cms\Form\Providers\MiddlewareServiceProvider;
-use Narsil\Cms\Form\Providers\MigrationServiceProvider;
-use Narsil\Cms\Form\Providers\ObserverServiceProvider;
-use Narsil\Cms\Form\Providers\PolicyServiceProvider;
-use Narsil\Cms\Form\Providers\RelationServiceProvider;
-use Narsil\Cms\Form\Providers\ResourceServiceProvider;
-use Narsil\Cms\Form\Providers\RouteServiceProvider;
-use Narsil\Cms\Form\Providers\TableServiceProvider;
-use Narsil\Cms\Form\Providers\ViewServiceProvider;
-use Nuwave\Lighthouse\LighthouseServiceProvider;
 
 #endregion
 
@@ -35,52 +14,5 @@ use Nuwave\Lighthouse\LighthouseServiceProvider;
  */
 class ServiceProvider extends BaseServiceProvider
 {
-    #region PUBLIC METHODS
-
-    /**
-     * Boot any application services.
-     *
-     * @return void
-     */
-    public function boot(): void
-    {
-        Model::preventLazyLoading(!App::isProduction());
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function register(): void
-    {
-        $this->registerProviders();
-    }
-
-    #endregion
-
-    #region PROTECTED METHODS
-
-    protected function registerProviders(): void
-    {
-        $this->app->register(CommandServiceProvider::class);
-        $this->app->register(ConfigurationServiceProvider::class);
-        $this->app->register(FieldServiceProvider::class);
-        $this->app->register(FormRequestServiceProvider::class);
-        $this->app->register(FormServiceProvider::class);
-        $this->app->register(FortifyServiceProvider::class);
-        $this->app->register(HorizonServiceProvider::class);
-        $this->app->register(LighthouseServiceProvider::class);
-        $this->app->register(LocalizationServiceProvider::class);
-        $this->app->register(MenuServiceProvider::class);
-        $this->app->register(MiddlewareServiceProvider::class);
-        $this->app->register(MigrationServiceProvider::class);
-        $this->app->register(ObserverServiceProvider::class);
-        $this->app->register(PolicyServiceProvider::class);
-        $this->app->register(RelationServiceProvider::class);
-        $this->app->register(ResourceServiceProvider::class);
-        $this->app->register(RouteServiceProvider::class);
-        $this->app->register(TableServiceProvider::class);
-        $this->app->register(ViewServiceProvider::class);
-    }
-
-    #endregion
+    //
 }
