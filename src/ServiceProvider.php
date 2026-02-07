d0e21a102bc15cd1e1439373cbc3e80665ea778f
Narsilien <rigaux.jonathan@gmail.com>
2026-02-07 10:21:50 +0100

    Fix menu translations with callbacks

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,6 +9,7 @@ use Narsil\Cms\Facades\Menus\Sidebar;
 use Narsil\Cms\Form\Models\Fieldset;
 use Narsil\Cms\Form\Models\Form;
 use Narsil\Cms\Form\Models\Input;
+use Narsil\Cms\Implementations\AbstractMenu;
 use Narsil\Cms\Providers\NarsilServiceProvider;
 use Narsil\Cms\Services\ModelService;
 use Narsil\Cms\Services\PermissionService;
@@ -75,34 +76,42 @@ class ServiceProvider extends NarsilServiceProvider
      */
     protected function bootSidebar(): void
     {
-        $group = trans('narsil-cms::ui.forms');
+        Sidebar::extend(function (AbstractMenu $menu): void
+        {
+            $group = trans('narsil-cms::ui.forms');
 
-        Sidebar::add(new MenuItem(Form::TABLE)
-            ->group($group)
-            ->icon('form')
-            ->label(ModelService::getTableLabel(Form::TABLE))
-            ->permissions([
-                PermissionService::getHandle(Form::TABLE, PermissionEnum::VIEW_ANY->value)
-            ])
-            ->route('forms.index'));
-
-        Sidebar::add(new MenuItem(Fieldset::TABLE)
-            ->group($group)
-            ->icon('fieldset')
-            ->label(ModelService::getTableLabel(Fieldset::TABLE))
-            ->permissions([
-                PermissionService::getHandle(Fieldset::TABLE, PermissionEnum::VIEW_ANY->value)
-            ])
-            ->route('fieldsets.index'));
-
-        Sidebar::add(new MenuItem(Input::TABLE)
-            ->group($group)
-            ->icon('input')
-            ->label(ModelService::getTableLabel(Input::TABLE))
-            ->permissions([
-                PermissionService::getHandle(Input::TABLE, PermissionEnum::VIEW_ANY->value)
-            ])
-            ->route('inputs.index'));
+            $menu
+                ->add(
+                    new MenuItem(Form::TABLE)
+                        ->group($group)
+                        ->icon('form')
+                        ->label(ModelService::getTableLabel(Form::TABLE))
+                        ->permissions([
+                            PermissionService::getHandle(Form::TABLE, PermissionEnum::VIEW_ANY->value)
+                        ])
+                        ->route('forms.index')
+                )
+                ->add(
+                    new MenuItem(Fieldset::TABLE)
+                        ->group($group)
+                        ->icon('fieldset')
+                        ->label(ModelService::getTableLabel(Fieldset::TABLE))
+                        ->permissions([
+                            PermissionService::getHandle(Fieldset::TABLE, PermissionEnum::VIEW_ANY->value)
+                        ])
+                        ->route('fieldsets.index')
+                )
+                ->add(
+                    new MenuItem(Input::TABLE)
+                        ->group($group)
+                        ->icon('input')
+                        ->label(ModelService::getTableLabel(Input::TABLE))
+                        ->permissions([
+                            PermissionService::getHandle(Input::TABLE, PermissionEnum::VIEW_ANY->value)
+                        ])
+                        ->route('inputs.index')
+                );
+        });
     }
 
     #endregion
