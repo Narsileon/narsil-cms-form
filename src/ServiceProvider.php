8573a44c6792848bcbe7935109f1b7249b60af07
Narsilien <rigaux.jonathan@gmail.com>
2026-02-07 07:17:23 +0100

    Boot sidebar

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,7 +4,15 @@ namespace Narsil\Cms\Form;
 
 #region USE
 
+use Narsil\Cms\Enums\Policies\PermissionEnum;
+use Narsil\Cms\Facades\Menus\Sidebar;
+use Narsil\Cms\Form\Models\Fieldset;
+use Narsil\Cms\Form\Models\Form;
+use Narsil\Cms\Form\Models\Input;
 use Narsil\Cms\Providers\NarsilServiceProvider;
+use Narsil\Cms\Services\ModelService;
+use Narsil\Cms\Services\PermissionService;
+use Narsil\Cms\Support\MenuItem;
 
 #endregion
 
@@ -29,6 +37,11 @@ class ServiceProvider extends NarsilServiceProvider
         $this->bootWebRoutes(__DIR__ . '/../routes/web.php');
 
         $this->bootMigrations();
+
+        $this->app->booted(function ()
+        {
+            $this->bootSidebar();
+        });
     }
 
     /**
@@ -53,6 +66,43 @@ class ServiceProvider extends NarsilServiceProvider
         $this->loadMigrationsFrom([
             __DIR__ . '/../database/migrations',
         ]);
+    }
+
+    /**
+     * Boot the sidebar.
+     *
+     * @return void
+     */
+    protected function bootSidebar(): void
+    {
+        $group = trans('narsil-cms::ui.forms');
+
+        Sidebar::add(new MenuItem()
+            ->group($group)
+            ->icon('form')
+            ->label(ModelService::getTableLabel(Form::TABLE))
+            ->permissions([
+                PermissionService::getHandle(Form::TABLE, PermissionEnum::VIEW_ANY->value)
+            ])
+            ->route('forms.index'));
+
+        Sidebar::add(new MenuItem()
+            ->group($group)
+            ->icon('fieldset')
+            ->label(ModelService::getTableLabel(Fieldset::TABLE))
+            ->permissions([
+                PermissionService::getHandle(Fieldset::TABLE, PermissionEnum::VIEW_ANY->value)
+            ])
+            ->route('fieldsets.index'));
+
+        Sidebar::add(new MenuItem()
+            ->group($group)
+            ->icon('input')
+            ->label(ModelService::getTableLabel(Input::TABLE))
+            ->permissions([
+                PermissionService::getHandle(Input::TABLE, PermissionEnum::VIEW_ANY->value)
+            ])
+            ->route('inputs.index'));
     }
 
     #endregion
